80b384fac939a35c56e24ce0ab218db406b6fae1
متجرالسوق الشامل <zainmuteea@gmail.com>
2026-09-20 18:51:29 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://wviryamcttjppbodqaal.supabase.co',
-  'sb_publishable_wG1BF08W47Di6SUkDjI3ag_zDT1crpx'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2aXJ5YW1jdHRqcHBib2RxYWFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4Njk0OTYsImV4cCI6MjEwNTQ0NTQ5Nn0.kLGCR8x8Rixr5GFegcgnyP-krzrP2cuiIfFdXBap_A0'
 )
