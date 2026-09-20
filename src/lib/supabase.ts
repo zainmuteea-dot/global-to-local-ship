38740cc49e2b74292b11c65c477c17741a2ce6a8
متجرالسوق الشامل <zainmuteea@gmail.com>
2026-09-20 08:10:27 +0300

    Initialize Supabase client in supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://wviryamcttjppbodqaal.supabase.co',
+  'sb_publishable_wG1BF08W47Di6SUkDjI3ag_zDT1crpx'
+)
