008161e5f00a81ab0c315c0f133ede449e56f2e1
متجرالسوق الشامل <zainmuteea@gmail.com>
2026-09-22 03:56:50 +0300

    Clean up comment in __root.tsx
    Removed comment about nested routes and Outlet.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -125,7 +125,6 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
       <AIRobot />
     </QueryClientProvider>
